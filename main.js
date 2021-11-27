bdbece411ab313655005e1cbb39e22dd53000cbc
Ahmed Obad <63117335+amo4oma@users.noreply.github.com>
2021-11-28 01:46:23 +0800

    adding all files

--- main.js
+++ main.js
@@ -6,7 +6,32 @@ $('.week').click(()=>{$('.duration').html('week');$('.dropdown-content').hide() 
 $('.month').click(()=>{$('.duration').html('month');$('.dropdown-content').hide() })
 $('.year').click(()=>{$('.duration').html('year');$('.dropdown-content').hide() })
 
+//revenue calculator
 
+$(document).ready(function(){
+  $('.cal-input').keypress(function(e){
+    if(e.keyCode==13)
+    $('.ravenue-btn').click();
+  });
+});
+
+
+function calculate(){
+  var input = $('.cal-input').val();
+  
+  var result = input * 1.2 
+  var comma  = result.toLocaleString()
+  console.log(comma);
+ $('.result').html(comma);
+ $('.gradian').css('left', '0%');
+ $('.result-h1').css('color', 'rgba(24, 17, 58, 0.9)');
+}
+ 
+ 
+ 
+ 
+ 
+ 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     autoplay: {
