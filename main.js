f6157386387d07667c1562dbae36fa72e9a60b01
Ahmed Obad <63117335+amo4oma@users.noreply.github.com>
2021-11-29 20:18:57 +0800

    icons

--- main.js
+++ main.js
@@ -117,3 +117,23 @@ const swiper = new Swiper('.swiper', {
 
   
   });
+
+
+  //Getting users location 
+
+  
+function ipLookUp () {
+  $.ajax('http://ip-api.com/json')
+  .then(
+      function success(response) {
+          console.log('User\'s Location Data is ', response);
+          console.log('User\'s Country', response.country);
+      },
+
+      function fail(data, status) {
+          console.log('Request failed.  Returned status of',
+                      status);
+      }
+  );
+}
+ipLookUp()
